1559d519d90637a6f2978cf7280b6e63cbca4b81
chdan-hub <ksmii358@gmail.com>
2025-08-03 02:00:45 -0700

    3회차 완료

---
+++ 3회차/회원가입 폼 만들기/join.js
@@ -0,0 +1,57 @@
+const form = document.getElementById("form")
+
+form.addEventListener("submit", function(e){
+  e.preventDefault();
+
+  let userId = e.target.id.value
+  let userPw1 = e.target.pw1.value
+  let userPw2 = e.target.pw2.value
+  let userName = e.target.name.value
+  let userPhone = e.target.phone.value
+  let userPosition = e.target.position.value
+  let userGender = e.target.gender.value
+  let userEmail = e.target.email.value
+  let userIntro = e.target.intro.value
+
+  console.log(userId, userPw1, userPw2, userName,
+    userPhone, userPosition, userGender, userEmail, userIntro)
+
+  if(userId.length < 6){
+    alert("아이디가 너무 짧습니다. 6자 이상 입력해주세요.")
+    return;
+  }
+
+  if(userPw1 !== userPw2){
+    alert("비밀번호가 일치하지 않습니다.")
+    return;
+  }
+
+  document.body.innerHTML = ""
+
+  let positionText;
+  switch(userPosition){
+    case "developer":
+      positionText = "개발자";
+      break;
+    case "designer":
+      positionText = "발개자";
+      break;
+    case "manager":
+      positionText = "자발개";
+      break;
+    default:
+      positionText = "미정";
+      break;
+  }
+
+  document.write(`
+    <h2>${userId}님 환영합니다!</h2>
+    <p>회원 가입 시 입력하신 내역은 다음과 같습니다.</p>
+    <ul>
+      <li>아이디: ${userId}</li>
+      <li>이름: ${userName}</li>
+      <li>전화번호: ${userPhone}</li>
+      <li>원하는 직무: ${positionText}</li>
+    </ul>
+  `);
+})
